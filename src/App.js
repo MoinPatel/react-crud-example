ce6f3c43a2002913747417ab87fe3cd650e887af
MoinPatel <moinpatel1992@gmail.com>
2021-08-30 22:18:09 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Create from './components/create';
+import Read from './components/read';
+import Update from './components/update';
+
+function App() {
+  return (
+    <Router>
+      <div className="main">
+        <h2 className="main-header">React Crud Operations</h2>
+
+        <div>
+          <Route exact path='/create' component={Create} />
+        </div>
+        <div style={{ marginTop: 20 }}>
+          <Route exact path='/read' component={Read} />
+        </div>
+
+        <Route path='/update' component={Update} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
